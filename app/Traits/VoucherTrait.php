503eff90c681be00ded1ef03cec5e1a1516dc7c7
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-03-04 02:36:44 +0700

    fix function get available voucher for customer

--- app/Traits/VoucherTrait.php
+++ app/Traits/VoucherTrait.php
@@ -2,7 +2,9 @@
 
 namespace App\Traits;
 
+use App\Http\Helpers\Constant;
 use App\Repositories\VoucherRepository;
+use Illuminate\Support\Carbon;
 
 trait VoucherTrait
 {
@@ -12,5 +14,22 @@ trait VoucherTrait
         $repo->setCustomerId($customerId);
         $repo->setSelectedField($selectedField);
         return $repo->detail();
+    }
+
+    public static function availableVoucher(array $selectedField)
+    {
+        $repo = new VoucherRepository();
+        $repo->setSelectedField($selectedField);
+        return $repo->available();
+    }
+
+    public static function lockVoucher($customerId, $availableVoucher)
+    {
+        $now = Carbon::now();
+        $availableVoucher->customer_id     = $customerId;
+        $availableVoucher->submission_time = $now->toDateTimeString();
+        $availableVoucher->lockdown_time   = $now->addMinutes(env('LOCKDOWN_MINUTES'))->toDateTimeString();
+        $availableVoucher->is_locked       = Constant::VOUCHER_SUBMISSION;
+        $availableVoucher->save();
     }
 }

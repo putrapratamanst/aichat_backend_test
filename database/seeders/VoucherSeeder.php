f73f330bfde644c5fb005c7140fc35862e77b38c
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-03-03 23:53:44 +0700

    fix seeder generate randomly 1000 voucher

---
+++ database/seeders/VoucherSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+
+class VoucherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        $faker = Faker::create();
+
+        for ($i = 1; $i <= 1000; $i++) {
+            // insert data to voucher table use Faker
+            DB::table('voucher')->insert([
+                'code'      => $faker->regexify("[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}"),
+                'is_locked' => false,
+                'submission_time' => "00:00:00",
+            ]);
+        }
+    }
+}

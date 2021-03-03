259a8a019e19cea2673f801762382e6e20cd2275
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-03-03 23:53:13 +0700

    file migration voucher

---
+++ database/migrations/2021_03_03_154102_create_voucher_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoucherTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('voucher', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->string("code")->unique();
+            $table->string("is_locked");
+            $table->time("submission_time")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('voucher');
+    }
+}

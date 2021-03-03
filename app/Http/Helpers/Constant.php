fb48eace2150027cf279d59cc351cce44e8d0840
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-03-04 01:39:04 +0700

    constant voucher status lock

---
+++ app/Http/Helpers/Constant.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Helpers;
+
+
+class Constant
+{
+    const VOUCHER_ACTIVE = 1;
+    const VOUCHER_GENERATED = 0;
+    const VOUCHER_SUBMISSION = 2;
+    const VOUCHER_INACTIVE = 3;
+}

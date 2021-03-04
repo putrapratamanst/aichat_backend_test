0f8f5ad5d298a04b7bafdfc6b3d20d9aeb04b506
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-03-04 15:24:33 +0700

    get detail voucher by customer id

--- app/Repositories/VoucherRepository.php
+++ app/Repositories/VoucherRepository.php
@@ -40,6 +40,17 @@ class VoucherRepository
             ->first();
     }
 
+    public function detail()
+    {
+        return Voucher::select($this->selectedField)
+            ->where('is_locked', Constant::VOUCHER_SUBMISSION)
+            ->where('customer_id', $this->customerId)
+            ->where('submission_time','!=', NULL)
+            ->where('lockdown_time', '!=', NULL)
+            ->whereRaw("!FIND_IN_SET({$this->customerId},exception)")
+            ->first();
+    }
+
     public function listUsed()
     {
         return Voucher::select($this->selectedField)
--- app/Traits/VoucherTrait.php
+++ app/Traits/VoucherTrait.php
@@ -31,6 +31,14 @@ trait VoucherTrait
         return $repo->listUsed();
     }
 
+    public static function detailVoucher($customerId, array $selectedField)
+    {
+        $repo = new VoucherRepository();
+        $repo->setCustomerId($customerId);
+        $repo->setSelectedField($selectedField);
+        return $repo->detail();
+    }
+
     public static function lockVoucher($customerId, $availableVoucher)
     {
         $now = Carbon::now();
@@ -39,5 +47,28 @@ trait VoucherTrait
         $availableVoucher->lockdown_time   = $now->addMinutes(env('LOCKDOWN_MINUTES'))->toDateTimeString();
         $availableVoucher->is_locked       = Constant::VOUCHER_SUBMISSION;
         $availableVoucher->save();
+    }
+
+
+    public static function activateVoucher($model)
+    {
+        $model->is_locked = Constant::VOUCHER_ACTIVE;
+        $model->save();
+    }
+
+
+    public static function setFreeVoucher($model)
+    {
+        $exception = $model->exception;
+        $exception .= $model->customer_id;
+        $exception .= ",";
+
+        $model->exception       = $exception;
+        $model->customer_id     = NULL;
+        $model->submission_time = NULL;
+        $model->lockdown_time   = NULL;
+        $model->is_locked       = Constant::VOUCHER_GENERATED;
+        $model->save();
+
     }
 }
